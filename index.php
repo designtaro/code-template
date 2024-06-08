14087f5fd82e3367ce39d58afd6b6bc31a44dbd2
ono_no1 <like.wear.business@gmail.com>
2024-06-08 22:27:08 +0900

    This theme is called happy. commit first

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php get_header(); ?>
-<main>
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <p>No posts found</p>
-    <?php endif; ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
 
+<?php get_footer(); ?>
